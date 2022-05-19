3dafe8e56569f78fb0e94951384834f829a7ef0f
Javier Gongora <jgongora@gmail.com>
2022-05-19 16:41:21 -0700

    substitute anchor component by react-router-dom Link

--- src/Pet.js
+++ src/Pet.js
@@ -1,20 +1,24 @@
+import { Link } from "react-router-dom";
+
 const Pet = ({ name, animal, breed, images, location, id }) => {
   let hero = "http://pet-images.dev-apis.com/pets/none.jpg";
   if (images.length) {
     hero = images[0];
   }
+
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
       <div className="info">
         <h1>{name}</h1>
-        <h2>it
+        <h2>
+          it
           {animal} — {breed} — {location}
         </h2>
       </div>
-    </a>
+    </Link>
   );
 };
 
