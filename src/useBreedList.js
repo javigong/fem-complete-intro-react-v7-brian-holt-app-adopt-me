1f9a64026e0ce04d08bfd9aac415119beb92ddd5
Javier Gongora <jgongora@gmail.com>
2022-05-19 16:40:20 -0700

    add useDebugValue to debug useBreedList custom Hook

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,10 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useDebugValue } from "react";
 
 const localCache = {};
 
 export default function useBreedList(animal) {
   const [breedList, setBreedList] = useState([]);
   const [status, setStatus] = useState("unloaded");
+  useDebugValue("number of values in cache: " + Object.keys(localCache).length);
+  console.log(localCache);
 
   useEffect(() => {
     if (!animal) {
